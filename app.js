f7df7ef690c9c18a5340b1c2e8e8a60408242587
Tioreza Febrian <tiorezafk@yahoo.com>
2018-01-01 02:03:56 +0700

    init koa

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const app = new Koa();
 
 var port = '8000';
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
 });
 
 app.listen(port,() => {
